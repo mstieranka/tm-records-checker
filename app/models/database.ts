bc3c54e7d482c36cfcad9cb06cbd69d8f62a74ee
Matej Stieranka <matej@stieranka.eu>
2024-06-04 19:12:43 +0200

    Force DB creation during migration

--- app/models/database.ts
+++ app/models/database.ts
@@ -2,5 +2,9 @@ import { drizzle } from 'drizzle-orm/bun-sqlite';
 import { Database } from 'bun:sqlite';
 import { resolveSync } from 'bun';
 
-const sqlite = new Database(resolveSync('./data/sqlite.db', process.cwd()));
+const sqlite = new Database(resolveSync('./data/sqlite.db', process.cwd()), {
+  create: false,
+  readwrite: true,
+});
 export const db = drizzle(sqlite);
+git
