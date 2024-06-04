c6ab74d0de5307284ff798b69ff73c28d2d070f4
Matej Stieranka <matej@stieranka.eu>
2024-06-04 19:02:42 +0200

    Add error checking to DB migration script

--- migrate.ts
+++ migrate.ts
@@ -2,8 +2,21 @@ import { migrate } from 'drizzle-orm/bun-sqlite/migrator';
 
 import { drizzle } from 'drizzle-orm/bun-sqlite';
 import { Database } from 'bun:sqlite';
+import { file } from 'bun';
+import { accessSync, constants } from 'node:fs';
 
-const sqlite = new Database('./data/sqlite.db');
-const db = drizzle(sqlite);
-await migrate(db, { migrationsFolder: './drizzle' });
-console.log('Migration complete');
+const dbFilePath = './data/sqlite.db';
+const dbFile = file(dbFilePath);
+if (!(await dbFile.exists())) {
+  console.log('Database file does not exist, creating...');
+}
+try {
+  accessSync(dbFilePath, constants.R_OK | constants.W_OK);
+  const sqlite = new Database('./data/sqlite.db');
+  const db = drizzle(sqlite);
+  await migrate(db, { migrationsFolder: './drizzle' });
+  console.log('Migration complete');
+} catch (err) {
+  console.error('Error migrating database:', err);
+  process.exit(1);
+}
