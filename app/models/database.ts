374f8edee42afb4af0c72a2d74bb864fe97e723c
Matej Stieranka <matej@stieranka.eu>
2024-06-04 18:57:26 +0200

    Improve database file resolution

--- app/models/database.ts
+++ app/models/database.ts
@@ -1,5 +1,6 @@
 import { drizzle } from 'drizzle-orm/bun-sqlite';
 import { Database } from 'bun:sqlite';
+import { resolveSync } from 'bun';
 
-const sqlite = new Database('./data/sqlite.db');
+const sqlite = new Database(resolveSync('./data/sqlite.db', process.cwd()));
 export const db = drizzle(sqlite);
