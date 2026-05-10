defeed97ff141b507f301b016a00cfaab2f42703
Matej Stieranka <matej@stieranka.eu>
2026-05-10 23:53:19 +0200

    fix: add server-side Bun handler

---
+++ app/entry.server.tsx
@@ -0,0 +1,31 @@
+import { isbot } from "isbot";
+import type { EntryContext } from "react-router";
+import { ServerRouter } from "react-router";
+import { renderToReadableStream } from "react-dom/server";
+
+export default async function handleRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  routerContext: EntryContext,
+) {
+  const stream = await renderToReadableStream(
+    <ServerRouter context={routerContext} url={request.url} />,
+    {
+      onError(error: unknown) {
+        responseStatusCode = 500;
+        console.error(error);
+      },
+    },
+  );
+
+  if (isbot(request.headers.get("user-agent"))) {
+    await stream.allReady;
+  }
+
+  responseHeaders.set("Content-Type", "text/html");
+  return new Response(stream, {
+    headers: responseHeaders,
+    status: responseStatusCode,
+  });
+}
